4d91d5fc1a24024f8abc0ee2f23509dee3218a97
Xpamoff <2005cramar.05@gmail.com>
2022-09-25 18:07:16 +0400

    It should be working now

--- ege-rus-accent-front/src/Button.js
+++ ege-rus-accent-front/src/Button.js
@@ -1,11 +1,11 @@
 import './Button.css';
 
-function Button({value, color="purple"}) {
+function Button({value, color="purple", onClick}) {
   const jsxStyle = {
     background: 'var(--' + color + ')'
   }
   return (
-    <div className="Button" style={jsxStyle}>
+    <div className="Button" style={jsxStyle} onClick={() => onClick()}>
         {value}
     </div>
   );
